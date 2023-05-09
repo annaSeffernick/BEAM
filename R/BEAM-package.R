5a9ebc03bd75cd6845fc803ce57ac86405d8b201
Anna Eames Seffernick <seffernick.13@buckeyemail.osu.edu>
2023-05-09 12:59:50 -0500

    Added subset_beam_result function; Updated package dependencies

---
+++ R/BEAM-package.R
@@ -0,0 +1,21 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr full_join
+#' @importFrom dplyr inner_join
+#' @importFrom magrittr %>%
+#' @importFrom MASS ginv
+#' @importFrom purrr reduce
+#' @importFrom stats coef
+#' @importFrom stats lm
+#' @importFrom stats nlminb
+#' @importFrom stats p.adjust
+#' @importFrom stats prcomp
+#' @importFrom stats predict
+#' @importFrom stats sd
+#' @importFrom survival coxph
+#' @importFrom survival coxph.control
+#' @importFrom survival Surv
+## usethis namespace: end
+NULL
